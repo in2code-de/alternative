2d54bf7d2ca8848c85f2b7bead4aad74f9becd08
Alexander Kellner <alexander.kellner@in2code.de>
2025-12-07 19:56:31 +0100

    [TASK] Extend eventListener to support TYPO3 13 and 14

--- Classes/EventListeners/AddFileButtonListener.php
+++ Classes/EventListeners/AddFileButtonListener.php
@@ -8,9 +8,11 @@ use In2code\Alternative\Utility\BackendUtility;
 use In2code\Alternative\Utility\ConfigurationUtility;
 use In2code\Alternative\Utility\FileUtility;
 use TYPO3\CMS\Backend\Routing\UriBuilder;
+use TYPO3\CMS\Backend\Template\Components\ActionGroup;
 use TYPO3\CMS\Backend\Template\Components\Buttons\LinkButton;
 use TYPO3\CMS\Core\Imaging\IconFactory;
 use TYPO3\CMS\Core\Imaging\IconSize;
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Resource\File;
 use TYPO3\CMS\Core\Resource\ResourceInterface;
 use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
@@ -31,24 +33,62 @@ class AddFileButtonListener
     public function __invoke(ProcessFileListActionsEvent $event): void
     {
         if ($this->isActivated($event->getResource())) {
+            $this->addButton($event);
+            $this->addButtonLegacy($event);
+        }
+    }
+
+    protected function addButton(ProcessFileListActionsEvent $event): void
+    {
+        // Todo: If condition can be always true (removed) once TYPO3 13 support is dropped
+        if ((new Typo3Version())->getMajorVersion() > 13) {
+            $componentGroup = $event->getActionGroup(ActionGroup::primary);
+            $componentGroup->add('metadata2', $this->getButton($event, $componentGroup->get('metadata')));
+        }
+    }
+
+    protected function getButton(ProcessFileListActionsEvent $event, LinkButton $button): LinkButton
+    {
+        $buttonNew = clone $button;
+        return $buttonNew
+            ->setTitle(LocalizationUtility::translate(
+                'LLL:EXT:alternative/Resources/Private/Language/Backend/locallang.xlf:button.translate')
+            )
+            ->setIcon($this->iconFactory->getIcon('actions-translate', IconSize::SMALL))
+            ->setHref(
+                (string)$this->uriBuilder->buildUriFromRoute(
+                    'alternative_Module.Module_addMetadata',
+                    ['file' => $event->getResource()->getUid()]
+                )
+            );
+    }
+
+    /**
+     * Todo: Can be removed once TYPO3 13 support is dropped
+     *
+     * @param ProcessFileListActionsEvent $event
+     * @return void
+     */
+    protected function addButtonLegacy(ProcessFileListActionsEvent $event): void
+    {
+        if ((new Typo3Version())->getMajorVersion() === 13) {
             $actionItems = $event->getActionItems();
             $actionItems = $this->insertArrayIntoArray(
                 $actionItems,
-                ['metadata2' => $this->getButton($event->getResource(), $actionItems['metadata'])]
+                ['metadata2' => $this->getButton($event, $actionItems['metadata'])]
             );
             $event->setActionItems($actionItems);
         }
     }
 
-    protected function getButton(ResourceInterface $resource, LinkButton $button): LinkButton
-    {
-        $buttonNew = clone $button;
-        return $buttonNew
-            ->setTitle(LocalizationUtility::translate('LLL:EXT:alternative/Resources/Private/Language/Backend/locallang.xlf:button.translate'))
-            ->setIcon($this->iconFactory->getIcon('actions-translate', IconSize::SMALL))
-            ->setHref($this->uriBuilder->buildUriFromRoute('alternative_Module.Module_addMetadata', ['file' => $resource->getUid()]));
-    }
-
+    /**
+     * Todo: Can be removed once TYPO3 13 support is dropped
+     *
+     * @param array $existingArray
+     * @param array $addArray
+     * @param string $after
+     * @return array
+     */
     protected function insertArrayIntoArray(array $existingArray, array $addArray, string $after = 'metadata'): array
     {
         if (array_key_exists($after, $existingArray) === false) {
